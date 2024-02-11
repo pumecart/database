c7b8f4e44c53d305d9bbae24cfc1e451b16f570b
Pumehana Cartagena <pum3hanacarta@byui.edu>
2024-02-10 20:02:02 -0500

    update to allow file to run

--- db-sql-code.sql
+++ db-sql-code.sql
@@ -1,8 +1,8 @@
 CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
-ALTER TYPE public.account_type
-    OWNER TO cse340pc;
+-- ALTER TYPE public.account_type
+--     OWNER TO cse340pc;
 
 --Table structure for table 'classification'
 CREATE TABLE public.classification (
